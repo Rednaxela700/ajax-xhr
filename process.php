04463b5f36f3851f2ae3d8d00f89d1c697a3ac7a
rednaxela700 <ohleg@windowslive.com>
2020-01-15 02:03:04 +0100

    copied $_GET name changed to POST

--- process.php
+++ process.php
@@ -5,3 +5,7 @@ echo 'Processing...';
 if(isset($_GET['name'])) {
     echo 'GET: Your name is '. $_GET['name'];
 }
+//Check for a get variable
+if(isset($_POST['name'])) {
+    echo 'POST: Your name is '. $_POST['name'];
+}
